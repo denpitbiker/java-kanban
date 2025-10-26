a2c970b88100e55920b80f2f9a160182a8b88f15
vdenk <dvoronkovporsche@gmail.com>
2025-10-26 14:01:34 +0300

    Codestyle....

--- src/ru/yandex/javacourse/schedule/network/gson/typetoken/EpicListTypeToken.java
+++ src/ru/yandex/javacourse/schedule/network/gson/typetoken/EpicListTypeToken.java
@@ -2,7 +2,6 @@ package ru.yandex.javacourse.schedule.network.gson.typetoken;
 
 import com.google.gson.reflect.TypeToken;
 import ru.yandex.javacourse.schedule.tasks.Epic;
-import ru.yandex.javacourse.schedule.tasks.Task;
 
 import java.util.List;
 
